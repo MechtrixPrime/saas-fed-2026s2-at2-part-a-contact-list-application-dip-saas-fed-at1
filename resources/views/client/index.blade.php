43b5d00876ef76276e215f7ea33fd0f0b102d9d7
John Robertson <john@myjamjar.com.au>
2026-08-11 09:24:39 +0800

    feat: last stages of app layout

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-zinc-800 leading-tight">
+            {{ __('Contacts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-8 grid grid-cols-1 space-y-4 max-w-7xl mx-auto">
+        <div class="grow w-full sm:px-6 lg:px-8">
+
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-zinc-900">
+                    Contact actions (add, search/filter)
+                </div>
+            </div>
+
+            <!-- repeat for each contact -->
+
+            <!-- contact card -->
+            {{-- @dd($variable) --}}
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-zinc-900">
+                    Contact Card here
+                </div>
+            </div>
+            <!-- /contact card -->
+
+            <!-- end repeat -->
+
+        </div>
+    </div>
+</x-app-layout>
