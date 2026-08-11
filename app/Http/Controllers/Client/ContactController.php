940cab18d4db7731b019116bdad0e6019f0fb512
John Robertson <john@myjamjar.com.au>
2026-08-11 10:58:14 +0800

    feat: finish contact routes

--- app/Http/Controllers/Client/ContactController.php
+++ app/Http/Controllers/Client/ContactController.php
@@ -12,7 +12,6 @@ final class ContactController
     public function index(): View
     {
         // dd($name);
-        // return "Welcome to the index method in the contact controller {$name}";
         $contact = [
             'id' => 1,
             'name' => 'Alice Smith',
@@ -23,6 +22,49 @@ final class ContactController
 
         return view('client.contacts.index', ['contact' => $contact]);
     }
+
+    public function show(): View
+    {
+        // dd($name);
+        $contact = [
+            'id' => 1,
+            'name' => 'Alice Smith',
+            'email' => 'alice@example.com',
+            'phone' => '0412 345 678',
+            'active' => true,
+        ];
+
+        return view('client.contacts.show', ['contact' => $contact]);
+    }
+
+    public function create(): View
+    {
+        // dd($name);
+        $contact = [
+            'id' => 1,
+            'name' => 'Alice Smith',
+            'email' => 'alice@example.com',
+            'phone' => '0412 345 678',
+            'active' => true,
+        ];
+
+        return view('client.contacts.create', ['contact' => $contact]);
+    }
+
+    public function edit(): View
+    {
+        // dd($name);
+        $contact = [
+            'id' => 1,
+            'name' => 'Alice Smith',
+            'email' => 'alice@example.com',
+            'phone' => '0412 345 678',
+            'active' => true,
+        ];
+
+        return view('client.contacts.edit', ['contact' => $contact]);
+    }
+
 
     // public function read_contact($id)
     // {
--- routes/web.client.php
+++ routes/web.client.php
@@ -7,3 +7,9 @@ use App\Http\Controllers\Client\ContactController;
 
 
 Route::get('/contacts', [ContactController::class, 'index']);
+Route::get('/contacts/create', [ContactController::class, 'create']);
+Route::get('/contacts/{contact}', [ContactController::class, 'show']);
+Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit']);
+// Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
+// Route::post('/contacts/{contact}', [ContactController::class, 'create']);
+// Route::patch('/contacts/{contact}', [ContactController::class, 'update']);
