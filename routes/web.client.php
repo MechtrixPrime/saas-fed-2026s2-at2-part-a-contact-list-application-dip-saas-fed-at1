4b7c628a435eecef6251fa8d40c8389e141a4a56
John Robertson <john@myjamjar.com.au>
2026-08-11 09:40:51 +0800

    feat: move routes

--- routes/web.client.php
+++ routes/web.client.php
@@ -2,13 +2,8 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/web.client.php';
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/contacts', [ContactController::class, 'index']);
