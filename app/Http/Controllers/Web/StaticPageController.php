a65f3d130caef61228addbbabc0c9dd9c1c3ba78
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2026-08-11 12:29:42 +0800

    feat: setup about blade

--- app/Http/Controllers/Web/StaticPageController.php
+++ app/Http/Controllers/Web/StaticPageController.php
@@ -7,4 +7,10 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
 
-final class StaticPageController extends Controller {}
+final class StaticPageController extends Controller
+{
+    public function about(): View
+    {
+        return view("web.static.about");
+    }
+}
